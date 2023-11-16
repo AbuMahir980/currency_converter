d2d3d0c73fadaa6425a0dfb3ee2bd348ca716f9d
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-16 22:01:41 +0100

    Add new changes'

--- script.js
+++ script.js
@@ -1,85 +1,43 @@
-const getCurrency = async () => {
-    const response = await fetch(`https://api.frankfurter.app/currencies`);
-    // console.log(response);
-    const data = await response.json();
-    // console.log(data);
-    return data;
+const input = document.querySelector('input[type=text]');
+// const fromCurrency = document.querySelector('#fromCurrency')
+// const toCurrency = document.querySelector("#toCurrency")
+
+const btn = document.querySelector('button')
+
+// 
+
+const getExchangeFrom = async () => {
+  const response = await fetch(
+		"https://api.frankfurter.app/latest"
+	);
+  const data = await response.json()
+    // console.log(data.rates.USD)
+  return data.rates.USD;
+  //108.49
 }
-// getCurrency();
 
-const getCurrencyRate = async (fromCurrency, toCurrency) => {
-    const currencyConvertUrl = new URL(
-			"https://api.frankfurter.app/latest"
-		);
-
-    currencyConvertUrl.searchParams.append('from=USD', fromCurrency)
-    currencyConvertUrl.searchParams.append("to=ZAR", toCurrency);
-
-    const result = await fetch(currencyConvertUrl);
-    const json = await result.json();
-      // console.log(json.rates)
-    return (json.rates);
+const getExchangeTo = async () => {
+	const newResponse = await fetch("https://api.frankfurter.app/latest");
+	const newData = await newResponse.json();
+	// console.log(data.rates.ZAR)
+	return newData.rates.ZAR;
+  //19.8718
 };
+// getExchangeTo();
 
-// getCurrencyRate();
-// const countries = {
-//     AUD: "Australian Dollar",
-//     BGN: "Bulgarian Lev",
-//     BRL: "Brazilian Real",
-//     CAD: "Canadian Dollar",
-//     CHF: "Swiss Franc",
-//     CNY: "Chinese Renminbi Yuan",
-//     CZK: "Czech Koruna",
-//     DKK: "Danish Krone",
-//     EUR: "Euro",
-//     GBP: "British Pound",
-//     HKD: "Hong Kong Dollar",
-//     HUF: "Hungarian Forint",
-//     IDR: "Indonesian Rupiah",
-//     ILS: "Israeli New Sheqel",
-//     INR: "Indian Rupee",
-//     ISK: "Icelandic Króna",
-//     JPY: "Japanese Yen",
-//     KRW: "South Korean Won",
-//     MXN: "Mexican Peso",
-//     MYR: "Malaysian Ringgit",
-//     NOK: "Norwegian Krone",
-//     NZD: "New Zealand Dollar",
-//     PHP: "Philippine Peso",
-//     PLN: "Polish Złoty",
-//     RON: "Romanian Leu",
-//     SEK: "Swedish Krona",
-//     SGD: "Singapore Dollar",
-//     THB: "Thai Baht",
-//     TRY: "Turkish Lira",
-//     USD: "United States Dollar",
-//     ZAR: "South African Rand"
-// }
-// const currencyKeys = [...Object.keys(countries)]
-// console.log(currencyKeys)
-// const appendToSelect = (selectElement, optionItem) => {
-//     const optionElement = document.createElement('option');
-//     const optionKeys = currencyKeys;
-//     optionItem = optionKeys
-//     optionElement.value = optionItem
-//     optionElement.textContent = optionItem;
+// console.log(getExchangeFrom())
+btn.addEventListener('click', async() => {
+  const results = input.va
+  // console.log(results)
+  // input.value = '';
 
-//     selectElement.appendChild(optionElement);
-// };
-
-// appendToSelect();
-
-// // const populateSelectElement = (selectElement, optionList) => {
-// //   optionList.forEach(optionItem => {
-// //       appendOptionToSelect(selectElement, optionItem);
-// //   })
-// // };
-
-const setupCurrencies = async () => {
-      const fromCurrencyElem = document.querySelector('#fromCurrency');
-      const toCurrencyElem = document.querySelector("#toCurrency");
-
-      const currOptions = await getCurrency();
+  const exchangeFrom = await getExchangeFrom();
+  const amount = `${exchangeFrom * results}`
+  const exchangeTo = await getExchangeTo();
+  const exchangeRate = `${amount * exchangeTo}`
+  convertResult.textContent = exchangeRate;
+})
 
 
-}
+
+
